d26d20d3b167fd73465f5c013c6c061552d0a913
Avantika Singh <81291895+avantika-2001@users.noreply.github.com>
2022-12-16 23:47:25 +0530

    Create EulerMethod.c

---
+++ EulerMethod.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+float f(float x, float y)
+{
+    return x+y;
+}
+void main()
+{
+    float x0,y0,n,h,xn,yn,func;
+    printf("Enter the initial condition:\n");
+    printf("x0:");
+    scanf("%f",&x0);
+    printf("y0:");
+    scanf("%f",&y0);
+    printf("Enter the calculation point xn:");
+    scanf("%f",&xn);
+    printf("Enter the no. of steps:");
+    scanf("%f",&n);
+    h=abs(x0-y0)/n;
+    printf("x0\ty0\tfunc\tyn\n");
+    for(int i=0;i<n;i++)
+    {
+        func=f(x0,y0);
+        yn=y0+h*func;
+        printf("%f\t%f\t%f\t%f\n",x0,y0,func,yn);
+        y0=yn;
+        x0=x0+h;
+    }
+    printf("Estimated value at %f is %f",xn,yn);
+}
