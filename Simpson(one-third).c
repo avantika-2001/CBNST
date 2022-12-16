e230d46a01d556a0090ab4552c6ec81371dea966
Avantika Singh <81291895+avantika-2001@users.noreply.github.com>
2022-12-16 21:37:17 +0530

    Create Simpson(one-third).c

---
+++ Simpson(one-third).c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+float f(float x)
+{
+    return 1/(1+x*x);   //function
+}
+void main()
+{
+    float x0,xn,h;
+    int n;
+    printf("Enter the number of intervals:");
+    scanf("%d",&n);
+    printf("Enter upper limit:");
+    scanf("%f",&xn);
+    printf("Enter lower limit:");
+    scanf("%f",&x0);
+    h=(float)(xn-x0)/n;
+    //printf("%f",h);
+    if(n%2==1)
+    {
+        n+=1;
+    }
+    h=(float)(xn-x0)/n;
+    float s1=f(x0)+f(xn);
+    float s2=0.0;
+    float s3=0.0;
+    for(int i=1;i<n;i++)
+    {
+        if(i%2==0)
+        {
+            s2+=f(x0+i*h);
+        }
+        else
+        {
+            s3+=f(x0+i*h);
+        }
+    }
+    float ans=h*(s1+2*s2+4*s3)/3;
+    printf("Result after integration:%6.4f",ans);
+}
